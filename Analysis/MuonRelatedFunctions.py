396d72706119b3e890cc2f21e09eb6bb6be6503e
Valeria D'Amante <vdamante@cern.ch>
2026-05-20 23:11:53 +0200

    added resolution (not yet scare variations) 2

--- Analysis/MuonRelatedFunctions.py
+++ Analysis/MuonRelatedFunctions.py
@@ -7,93 +7,6 @@ if __name__ == "__main__":
 from FLAF.Common.Utilities import *
 
 
-def GetMuMuMassResolution(df, pt_to_use):
-    sigma_pt = {
-        "scare": "mu{0}_ptErr/mu{0}_pt",
-        "nano": "mu{0}_ptErr/mu{0}_pt",
-        "scare_reapplied": "mu{0}_ptErr/mu{0}_pt",
-        "BS": "mu{0}_bsConstrainedPtErr/mu{0}_bsConstrainedPt",
-        "BS_scare": "mu{0}_bsConstrainedPtErr/mu{0}_bsConstrainedPt",
-        "RoccoR": "mu{0}_ptErr/mu{0}_pt",
-        "BS_RoccoR": "mu{0}_bsConstrainedPtErr/mu{0}_bsConstrainedPt",
-    }
-    sigma_scaleandresol = {
-        "scare": "0.5*(((mu{0}_pt_1_corr_up-mu{0}_pt_1_scale_corr)*(mu{0}_pt_1_corr_up-mu{0}_pt_1_scale_corr))+((mu{0}_pt_1_corr_dn-mu{0}_pt_1_scale_corr)*(mu{0}_pt_1_corr_dn-mu{0}_pt_1_scale_corr)))",
-        "nano": "0.",
-        "scare_reapplied": "0.5*(((mu{0}_pt_1_corr_up-mu{0}_pt_1_scale_corr)*(mu{0}_pt_1_corr_up-mu{0}_pt_1_scale_corr))+((mu{0}_pt_1_corr_dn-mu{0}_pt_1_scale_corr)*(mu{0}_pt_1_corr_dn-mu{0}_pt_1_scale_corr)))",
-        "BS": "0.",
-        "BS_scare": "0.5*(((mu{0}_BS_pt_1_corr_up-mu{0}_BS_pt_1_corr)*(mu{0}_BS_pt_1_corr_up-mu{0}_BS_pt_1_corr))+((mu{0}_BS_pt_1_corr_dn-mu{0}_BS_pt_1_corr)*(mu{0}_BS_pt_1_corr_dn-mu{0}_BS_pt_1_corr)))",
-        "RoccoR": "0.",
-        "BS_RoccoR": "0.",
-    }
-    for mu_idx in [1, 2]:
-        # print(sigma_pt[pt_to_use].format(mu_idx))
-        # print(sigma_scaleandresol[pt_to_use].format(mu_idx))
-        df = df.Define(f"sigma_mu{mu_idx}_pt_rel", sigma_pt[pt_to_use].format(mu_idx))
-        # df.Display({f"sigma_mu{mu_idx}_pt_rel"}).Print()
-        # df=df.Define(f"sigma_mu{mu_idx}_pt_rel", f"sigma_mu{mu_idx}_pt_rel/mu{mu_idx}_pt") # is it alreadt relative??
-        df = df.Define(
-            f"sigma_mu{mu_idx}_scaleresolution",
-            sigma_scaleandresol[pt_to_use].format(mu_idx),
-        )
-        df = df.Define(
-            f"sigma_mu{mu_idx}_scaleresolution_rel",
-            f"sigma_mu{mu_idx}_scaleresolution/mu{mu_idx}_pt",
-        )
-        # df.Display({f"sigma_mu{mu_idx}_scaleresolution"}).Print()
-        df = df.Define(
-            f"sigma_mu{mu_idx}_total_pt_rel",
-            f"sqrt( pow(sigma_mu{mu_idx}_pt_rel,2) + sigma_mu{mu_idx}_scaleresolution_rel )",
-        )
-        # df.Display({f"sigma_mu{mu_idx}_total_pt_rel"}).Print()
-    delta_mu_expr = "0.5*sqrt({0}*{0}+{1}*{1}) "
-    # delta_mu_expr = "sqrt( 0.5 * (pow( ({0}/{1}), 2) + pow( ({2}/{3}), 2) ) ) "
-    df = df.Define(
-        "m_mumu_resolution",
-        delta_mu_expr.format("sigma_mu1_total_pt_rel", "sigma_mu2_total_pt_rel"),
-    )
-    # df.Display({"m_mumu_resolution"}).Print()
-    # df = df.Define(
-    #     "m_mumu_resolution_nano",
-    #     delta_mu_expr.format(
-    #         "mu1_ptErr",
-    #         "mu1_pt_nano",
-    #         "mu2_ptErr",
-    #         "mu2_pt_nano",
-    #     ),
-    # )
-    # df = df.Define(
-    #     "m_mumu_resolution",
-    #     delta_mu_expr.format(
-    #         "(mu1_pt-mu1_pt_nano)/mu1_pt",
-    #         "mu1_pt",
-    #         "(mu2_pt-mu2_pt_nano)/mu2_pt",
-    #         "mu2_pt",
-    #     ),
-    # )
-
-    # df = df.Define(
-    #     "m_mumu_resolution_BS",
-    #     delta_mu_expr.format(
-    #         "mu1_bsConstrainedPtErr",
-    #         "mu1_bsConstrainedPt",
-    #         "mu2_bsConstrainedPtErr",
-    #         "mu2_bsConstrainedPt",
-    #     ),
-    # )
-    # df = df.Define(
-    #     "m_mumu_resolution_BS_ScaRe",
-    #     delta_mu_expr.format(
-    #         "(mu1_BS_pt_1_corr-mu1_bsConstrainedPt)",
-    #         "mu1_BS_pt_1_corr",
-    #         "(mu2_BS_pt_1_corr-mu2_bsConstrainedPt)",
-    #         "mu2_BS_pt_1_corr",
-    #     ),
-    # )
-
-    return df
-
-
 def GetMuonP4Observables(df):
     for pt_suffix in [
         "",
@@ -236,7 +149,7 @@ def GetAllMuonsObservablesNew(df):
         },
     }  # TO BE FIXED WITH THE SCARE UNC INCLUSION!! --> add friend ttree for scare
 
-    for pt_suffix, pt_info in pT_variants.items():
+    for pt_suffix, pt_info in pt_variants.items():
         # Check if both muons have the required pT columns
         mu1_pt_name = pt_info["pt_name_template"].format(1)
         mu2_pt_name = pt_info["pt_name_template"].format(2)
